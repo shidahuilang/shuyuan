80f494605ce6b853ddef8e2c337e87f87c73bdaa
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2023-11-21 17:10:12 +0800

    Update book.py

--- book.py
+++ book.py
@@ -39,7 +39,7 @@ def parse_page():
                 print(f"Link: {href}, Date String: {link_date_str}, Calculated Date: {link_date}")
 
                 # Check if the link is within the specified time range
-                if 1 <= days_ago <= 3:  # Include links from 1 to 3 days ago
+                if 1 <= days_ago <= 4:  # Include links from 1 to 3 days ago
                     json_url = f'https://www.yckceo.com{href.replace("content", "json")}'
                     relevant_links.append((json_url, link_date))
 
