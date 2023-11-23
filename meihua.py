6626c42367031e9c8c9c365c368f9b9261fd766e
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2023-11-23 16:01:04 +0800

    Update meihua.py

--- meihua.py
+++ meihua.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-def format_json_files(input_folder, output_folder):
+def format_and_convert_unicode(input_folder, output_folder, replace_original=False):
     # 确保输出文件夹存在
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -15,13 +15,23 @@ def format_json_files(input_folder, output_folder):
             with open(input_file, 'r') as file:
                 json_data = file.read()
 
+            # 将JSON字符串解析为Python对象
             parsed_json = json.loads(json_data)
-            formatted_json = json.dumps(parsed_json, indent=4)
 
-            with open(output_file, 'w') as file:
+            # 使用json.dumps将Python对象转换为格式化的JSON字符串，同时处理Unicode转义字符
+            formatted_json = json.dumps(parsed_json, indent=4, ensure_ascii=False)
+
+            # 写入到输出文件夹
+            with open(output_file, 'w', encoding='utf-8') as file:
                 file.write(formatted_json)
 
-            print("已成功美化并保存文件：", output_file)
+            print("已成功转换Unicode并美化保存文件：", output_file)
+
+            # 替换原始文件（如果指定了替换原始文件的选项）
+            if replace_original:
+                os.remove(input_file)
+                os.rename(output_file, input_file)
+                print("已替换原始文件：", input_file)
 
 # 设置输入文件夹名字和输出文件夹名字
 input_folder_name = "json/input"
@@ -34,5 +44,5 @@ script_directory = os.path.dirname(os.path.abspath(__file__))
 input_folder = os.path.join(script_directory, input_folder_name)
 output_folder = os.path.join(script_directory, output_folder_name)
 
-# 调用函数美化JSON文件
-format_json_files(input_folder, output_folder)
+# 调用函数转换Unicode并美化JSON文件
+format_and_convert_unicode(input_folder, output_folder, replace_original=True)
