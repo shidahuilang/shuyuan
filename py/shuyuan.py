ae5d0fb18227cabe783001d7718741691bf87faa
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2023-11-25 20:06:26 +0800

    Update shuyuan.py

--- py/shuyuan.py
+++ py/shuyuan.py
@@ -18,8 +18,8 @@ urls = [
 
 # 定义不同网址对应的时间范围，单位为天
 time_ranges = {
-    'https://www.yckceo.com/yuedu/shuyuan/index.html': (1, 5),
-    'https://www.yckceo.com/yuedu/shuyuans/index.html': (1, 5),
+    'https://www.yckceo.com/yuedu/shuyuan/index.html': (1, 9),
+    'https://www.yckceo.com/yuedu/shuyuans/index.html': (1, 7),
 }
 
 def parse_page(url):
